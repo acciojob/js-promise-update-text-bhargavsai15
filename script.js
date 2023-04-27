e71e4bfb4e8b3b256d1189ef50652cd0c438d23a
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-28 00:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const ele=document.getElementById('output');
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hello, world!');
+  }, 1000);
+})
+
+promise.then((text)=>{
+	ele.innerText=text;
+})
